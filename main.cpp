cc844402a80a564e0fe8859c6874392d6e77a878
nguyentrungduc <nguyentrungduc08@gmail.com>
2018-07-27 19:02:06 +0700

    add debug test

--- main.cpp
+++ main.cpp
@@ -25,9 +25,12 @@
 #include <Poco/Notification.h>
 #include "Poco/AutoPtr.h"
 
+#define SIGDUM 8888
 
-Poco::NotificationQueue g_nofiQueue;
+Poco::NotificationQueue g_notiQueue;
 bool g_flag = true;
+
+//demo object data
 
 class Object {
 private:
@@ -35,10 +38,19 @@ private:
     std::string m_sdata;
 
 public:
-    Object(int in, std::string s) : m_idata(in), m_sdata(s) {
 
+    Object(int in, std::string s) : m_idata(in), m_sdata(s) {
+        std::cout << "!Create Object " << this->m_sdata << std::endl;
     }
     
+    Object(const Object &ob) {
+        std::cout << "!Copy constructor object " << this->m_sdata << std::endl;
+    }
+
+    ~Object() {
+        std::cout << "!Destroy Object " << this->m_sdata << std::endl;
+    }
+
     std::string getData() {
         return this->m_sdata;
     }
@@ -55,29 +67,35 @@ class Task : public Poco::Runnable {
 private:
     std::string m_sData;
 public:
-    Task(std::string s): m_sData(s) {
-        std::cout << "create task" << m_sData << std::endl;
+
+    Task(std::string s) : m_sData(s) {
+        std::cout << "-Create task " << m_sData << std::endl;
     }
-    
+
+    Task(const Task& obj) {
+        std::cout << "-Copy constructor task" << m_sData << std::endl;
+    }
+
     ~Task() {
-        std::cout << "destroy task" << m_sData << std::endl;
+        std::cout << "-Destroy task " << m_sData << std::endl;
     }
-    
+
     virtual void run() {
-        std::cout << m_sData <<  " do longer task" << std::endl;
+        std::cout << m_sData << " do longer task" << std::endl;
         Poco::Thread::sleep(1000);
-        std::cout << "finished task" << std::endl;
+        std::cout << m_sData << " finished task" << std::endl;
     }
 };
 
 class notiJob : public Poco::Notification {
 public:
 
-    notiJob(Object o) : m_oData(o) {
+    notiJob(const Object &o) : m_oData(o) {
+        std::cout << "+Create notification " << m_oData.getData() << std::endl;
     }
-
+    
     ~notiJob() {
-        std::cout << "destroy notification" << std::endl;
+        std::cout << "+Destroy notification " << m_oData.getData() << std::endl;
     }
 
     Object getData() {
@@ -89,51 +107,65 @@ private:
 };
 
 //This thread using for assign task for worker in pool
+
 class Distributor : public Poco::Runnable {
 public:
 
     virtual void run() {
-        std::cout << "distributor's job" << std::endl;
+        std::cout << "Distributor's job" << std::endl;
 
-        Poco::AutoPtr<Poco::Notification> pNoti(g_nofiQueue.waitDequeueNotification()); //
+        Poco::AutoPtr<Poco::Notification> pNoti(g_notiQueue.waitDequeueNotification()); //
         while (pNoti && g_flag) {
             notiJob* pWorkNf = dynamic_cast<notiJob*> (pNoti.get()); //get notification from NotificationQueue
             if (pWorkNf && Poco::ThreadPool::defaultPool().available() > 0) {
+                    
                 boost::shared_ptr<Task> worker(new Task(pWorkNf->getData().getData()));
+                
                 Poco::ThreadPool::defaultPool().start(*(worker.get())); // set task for Worker
-                std::cout << "$$$$check crash" << std::endl;
-                pNoti = g_nofiQueue.waitDequeueNotification(); // get Net nofitication if available in queue
+                
+                if (pWorkNf->getData().getData() == "object19"){
+                    return;
+                }
+                
+                pNoti = g_notiQueue.waitDequeueNotification(); // get Net nofitication if available in queue
             }
+//            std::cout << "log......\n";
         }
 
+        std::cout << "Distributor DONE" << std::endl;
     }
 };
 
 void handle_Signal(int signum) {
-    std::cout << "signal emit " << signum << std::endl;
+    std::cout << "-----------signal emit-----------" << signum << std::endl;
     g_flag = false;
 }
 
 int main() {
-    //    Poco::ThreadPool threadPool("threadPoolDemo",
-    //                                );
-//    signal(SIGSEGV, handle_Signal);
-
     Distributor di;
     Poco::Thread thread;
 
     thread.start(di);
+    
+//    sleep(1);
+
     int count = 0;
     while (count < 20) {
-        std::string s = "object" + std::to_string(count);
-        Object ob(count, s);
-        g_nofiQueue.enqueueNotification(new notiJob(ob));
+        std::string s = "object" + std::to_string(count); 
+        Object ob(count, s); // call contructor object
+        
+        /*
+         - call copy constructor object
+         - call constructor object notiJob
+         */
+        g_notiQueue.enqueueNotification(new notiJob(ob)); 
+        
         ++count;
     }
-    
-    sleep(10);
-//    raise(SIGSEGV);
-    
+
     thread.join();
+
+    Poco::ThreadPool::defaultPool().joinAll();
+
     return 0;
 }
